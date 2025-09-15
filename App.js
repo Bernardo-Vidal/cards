9f194ae2baaf8007b34b4b8eda605ddb8cab668b
Bernardo-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2025-09-15 10:40:18 -0300

    completo

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   ImageBackground,
   Button,
   Linking,
+  Pressable
 } from "react-native";
 
 export default function App() {
@@ -28,47 +29,44 @@ export default function App() {
 
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ backgroundColor: "#fff", flex: 1 }}>
-        <Text>dfkjhaskdjlfhsa</Text>
+      <View style={styles.titleBox}>
+        <Pressable style={{width:'100%', height:'100%'}} onPress={() => Linking.openURL("https://www.youtube.com/@vidal_57")}>
+          <Image source={require('./assets/pfp.jpg')} style={styles.pfp}></Image>
+          <Text style={styles.title}>Vidal57's YT Channel</Text>
+        </Pressable>
       </View>
       <View style={styles.container}>
-        <TouchableOpacity style={styles.box1} onPress={() => contador(1)}>
-          <Image source={require("./assets/favicon.png")} />
-          <Text style={styles.texto}>Box 1</Text>
+        <TouchableOpacity style={styles.box} onPress={() => Linking.openURL("https://www.youtube.com/watch?v=rmECgSdubeA")}>
+          <Image source={require('./assets/deadcells1.jpg')} style={styles.thumb}></Image>
+          <Text style={styles.texto}>Dead Cells #1</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.box2}
-          onPress={
-            (() => contador(2),
-            () => Linking.openURL("https://www.youtube.com/shorts/QMR8rlPCOJU"))
-          }
-        >
-          <Text style={styles.texto}>Vídeo do Youtube</Text>
+        <TouchableOpacity style={styles.box} onPress={() => Linking.openURL("https://www.youtube.com/watch?v=lRaN1X3ZcII")}>
+          <Image source={require('./assets/terraria5.jpg')} style={styles.thumb}></Image>
+          <Text style={styles.texto}>Terraria #5</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.box3} onPress={() => contador(3)}>
-          <Text style={styles.texto}>Box 3</Text>
+        <TouchableOpacity style={styles.box} onPress={() => Linking.openURL("https://www.youtube.com/watch?v=C7AwptESr1I")}>
+          <Image source={require('./assets/terraria4.jpg')} style={styles.thumb}></Image>
+          <Text style={styles.texto}>Terraria #4</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.box1} onPress={() => contador(4)}>
-          <Text style={styles.texto}>Box 4</Text>
+        <TouchableOpacity style={styles.box} onPress={() => Linking.openURL("https://www.youtube.com/watch?v=V1L2TnghorA&pp=0gcJCckJAYcqIYzv")}>
+          <Image source={require('./assets/terraria3.jpg')} style={styles.thumb}></Image>
+          <Text style={styles.texto}>Terraria #3</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.box2} onPress={() => contador(5)}>
-          <Text style={styles.texto}>Box 5</Text>
+        <TouchableOpacity style={styles.box} onPress={() => Linking.openURL("https://www.youtube.com/watch?v=1miXl3ab7fA")}>
+          <Image source={require('./assets/terraria2.jpg')} style={styles.thumb}></Image>
+          <Text style={styles.texto}>Terraria #2</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.box3} onPress={() => contador(6)}>
-          <Text style={styles.texto}>Box 6</Text>
+        <TouchableOpacity style={styles.box} onPress={() => Linking.openURL("https://www.youtube.com/watch?v=EZXc_D0hF7g&pp=0gcJCckJAYcqIYzv")}>
+          <Image source={require('./assets/terraria1.jpg')} style={styles.thumb}></Image>
+          <Text style={styles.texto}>Terraria #1</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.box1} onPress={() => contador(7)}>
-          <Text style={styles.texto}>Box 7</Text>
+        <TouchableOpacity style={styles.box} onPress={() => Linking.openURL("https://www.youtube.com/watch?v=hWnxVxVSpI4")}>
+          <Image source={require('./assets/ultrakill2.jpg')} style={styles.thumb}></Image>
+          <Text style={styles.texto}>ULTRAKILL #2</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.box2} onPress={() => contador(8)}>
-          <Text style={styles.texto}>Box 8</Text>
+        <TouchableOpacity style={styles.box} onPress={() => Linking.openURL("https://www.youtube.com/watch?v=H8E8DuIuLLo")}>
+          <Image source={require('./assets/ultrakill1.jpg')} style={styles.thumb}></Image>
+          <Text style={styles.texto}>ULTRAKILL #1</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -78,51 +76,72 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 6,
-    backgroundColor: "#ffc3c3ff",
+    backgroundColor: "#150055",
     alignItems: "center", //horizontal
     justifyContent: "center", //vertical
-    flexDirection: "column",
+    flexDirection: "row",
     flexWrap: "wrap", //quebra em linhas ou colunas
     alignContent: "space-evenly",
-    rowGap: 50,
+    columnGap: 20,
     //columnGap: 30
   },
-  title: {
-    backgroundColor: "#ffc3c3ff",
-    alignItems: "center", //horizontal
-    justifyContent: "center", //vertical
-    flexDirection: "column",
-    flexWrap: "wrap", //quebra em linhas ou colunas
-    alignContent: "space-evenly",
-    rowGap: 50,
+  titleBox:{
+    flex: 1,
+    backgroundColor: "#100066",
+    //alignItems: 'flex-start', //horizontal
+    //justifyContent: "center", //vertical
+    //flexDirection: 'column',
+    //flexWrap: 'wrap', //quebra em linhas ou colunas
+    //alignContent: 'flex-end',
+    //rowGap: 50,
+    paddingTop: 40,
+    borderWidth: 8,
+    borderColor: '#003066',
   },
-  box1: {
+  title:{
+    fontSize: 40,
+    fontWeight: 700,
+    color: '#fff',
+    padding: '2%',
+    paddingTop: 0
+    //alignItems: 'flex-end', //horizontal
+    //justifyContent: "center", //vertical
+    //flexDirection: 'column',
+    //flexWrap: 'wrap', //quebra em linhas ou colunas
+    //alignContent: 'flex-end',
+    //rowGap: 50,
+  },
+  box: {
     backgroundColor: "#57a0ff84",
-    width: 150,
+    width: 170,
     height: 150,
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 20,
-  },
-  box2: {
-    backgroundColor: "#57a0ff84",
-    width: 150,
-    height: 150,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 20,
-  },
-  box3: {
-    backgroundColor: "#57a0ff84",
-    width: 150,
-    height: 150,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 20,
+    borderRadius: 15,
+    paddingTop: 10
   },
   texto: {
+    flex:1,
     fontSize: 20,
     color: "#fff",
     fontWeight: 700,
+    paddingTop: 5
   },
+  thumb:{
+    flex: 2,
+    height: '90%',
+    width: '90%',
+    borderRadius: 25,
+    resizeMode: 'contain'
+  },
+  pfp:{
+    margin: '2%',
+    marginBottom:0,
+    height:70,
+    width:70,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: '#fff',
+    resizeMode: 'contain'
+  }
 });
